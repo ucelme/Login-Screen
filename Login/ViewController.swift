f82df115d455aca5c3076442cefcde4c0f256c52
Alex <alex@mail.ru>
2020-12-07 11:39:09 +0300

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Oleg Starkin on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
